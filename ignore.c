1a4acdd4b1b3f4f60e9f57749e5289cb208ec2e9
ChristopherLin4 <Chrislin135@gmail.com>
2023-12-02 16:24:18 -0500

    dsafsa

--- ignore.c
+++ ignore.c
@@ -13,7 +13,7 @@ int check3(int choice){
 
 
 int ignore(){
-        printf("When you get to school you notice a poster on the door of your first class, Professor Turd's class. You peek over the shoulders of the other students and read what is says. \nPop quiz, 50 percent of your grade \nYou feel doomed. What do you do?\n");
+        printf("\nWhen you get to school you notice a poster on the door of your first class, Professor Turd's class. You peek over the shoulders of the other students and read what is says. \nPop quiz, 50 percent of your grade \nYou feel doomed. What do you do?\n");
         printf("1. Leave the building and wait out till the next class\n2. Walk in and take the test\n Input a number:");
         //array
         char op1[169] = {'Y','o','u',' ','l','e','a','v','e',' ','a','n','d',' ','g','o',' ','t','o',' ','t','h','e',' ','l','i','b','r','a','r','y','.',' ','U','n','f','o','r','t','u','n','a','t','e','l','y',' ','P','r','o','f','e','s','s','o','r',' ','T','u','r','d',' ','f','a','i','l','s',' ','y','o','u',' ','f','o','r',' ','n','o','t',' ','s','h','o','w','i','n','g',' ','u','p',' ','a','n','d',' ','y','o','u',' ','a','r','e',' ','k','i','c','k','e','d',' ','f','r','o','m',' ','t','h','e',' ','C','o','o','p','e','r',' ','U','n','i','o','n','.',' ','O','h',' ','n','o','.',' ','D','o',' ','n','o','t',' ','s','k','i','p',' ','s','c','h','o','o','l',' ','k','i','d','s','.','\0'};
