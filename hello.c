0781481da329c36cb890b9c1eecbc3f700100d07
ChristopherLin4 <Chrislin135@gmail.com>
2023-12-02 10:10:07 -0500

    hello.c

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int helloworld(){
+        printf("Hello World!\n"); //Hello World
+        printf("You just woke up. \n You check the clock. \n it reads 8:55am. \n "Sigh" another long day at the Cooper Union. \n You get out of bed, shower, and brush your teeth.");
+        
+        
+
+        return 0;
+}
