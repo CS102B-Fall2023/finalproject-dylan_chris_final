8a839b21cca697f9c78b6d421b30a8c517993a16
ChristopherLin4 <Chrislin135@gmail.com>
2023-12-02 10:15:41 -0500

    new hello.c

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 
 int helloworld(){
         printf("Hello World!\n"); //Hello World
-        printf("You just woke up. \n You check the clock. \n it reads 8:55am. \n "Sigh" another long day at the Cooper Union. \n You get out of bed, shower, and brush your teeth.");
+        printf("You just woke up. \nYou check the clock. \nIt reads 8:55am. \nSigh \nAnother long day at the Cooper Union. \nYou get out of bed, shower, and brush your teeth.\n");
         
         
 
