47c9585e15afd8813fa8a8ef80bd3563ea1f74b4
ChristopherLin4 <Chrislin135@gmail.com>
2023-12-02 12:44:43 -0500

    ignore.c

--- ignore.c
+++ ignore.c
@@ -15,7 +15,7 @@ int check2(int choice){
 void ignore(){
         printf("You throw it into the trash and walk to school. When you get to school you notice a poster on the door of your first class, Professor Turd's class. You peek over the shoulders of the other students and read what is says. \nPop quiz, 50 percent of your grade \nYou feel doomed. What do you do?");
         printf("1. Leave the building and wait out till the next class\n2. Walk in and take the test\n Input a number:");
-        
+        //array
         char op1[164] = {Y,o,u, ,l,e,a,v,e, ,a,n,d, ,g,o, ,t,o, ,t,h,e, ,l,i,b,r,a,r,y,., ,U,n,f,o,r,t,u,n,a,t,e,l,y, ,P,r,o,f,e,s,s,o,r, ,T,u,r,d, ,f,a,i,l,s, ,y,o,u, ,f,o,r, ,n,o,t, ,s,h,o,w,i,n,g, ,u,p, ,a,n,d, ,y,o,u, ,a,r,e, ,k,i,c,k,e,d, ,f,r,o,m, ,t,h,e, ,C,o,o,p,e,r, ,U,n,i,o,n,., ,O,h, ,n,o,., ,D,o,n,’,t, ,s,k,i,p, ,s,c,h,o,o,l, ,k,i,d,s,.}
 
 	int choice;
@@ -26,7 +26,7 @@ void ignore(){
         while (getchar() != '\n');
         scanResult = scanf("%d", &choice);
     }
-	if (choice == 1){
+	if (choice == 1){//for loop
 	for(int i = 0; i < strlen(op1); i++){
                 printf("%s", op1[1]);
         }
